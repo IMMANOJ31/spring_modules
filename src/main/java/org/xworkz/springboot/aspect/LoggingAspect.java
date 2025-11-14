aacd74c53c7c60f613f0e55bc85719d2bc7d5308
IMMANOJ31 <manojkumard.21is@saividya.ac.in>
2025-11-14 12:39:07 +0530

    init commit

--- src/main/java/org/xworkz/springboot/aspect/LoggingAspect.java
+++ src/main/java/org/xworkz/springboot/aspect/LoggingAspect.java
@@ -1,10 +1,9 @@
 package org.xworkz.springboot.aspect;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.SourceLocation;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -15,17 +14,77 @@ public class LoggingAspect {
         System.err.println("Invoking logging aspect...");
     }
 
-    //@Pointcut
-
-    @Before("execution(* com.xworkz.springboot.service.*.*(..))")
-    public void logBefore(JoinPoint joinPoint) {
-        joinPoint.getSignature();
-
-        System.err.println("Logging BEFORE: " + joinPoint.getSignature());
+    @Pointcut("execution(* com.xworkz.springboot.service.*.*(..))")
+    public void allService(){
+        //marker method
     }
 
-    @AfterReturning("execution(* com.xworkz.springboot.service.*.*(..))")
+    @Before("allService()")
+    public void logBefore(JoinPoint joinPoint) {
+        Signature signature = joinPoint.getSignature();
+        Object[] args = joinPoint.getArgs();
+        String kind = joinPoint.getKind();
+        SourceLocation sourceLocation = joinPoint.getSourceLocation();
+        Object target = joinPoint.getTarget();
+        JoinPoint.StaticPart staticPart = joinPoint.getStaticPart();
+        String longString = joinPoint.toLongString();
+        String shortString = joinPoint.toShortString();
+        String string = joinPoint.toString();
+
+        System.out.println("Signature: "+signature);
+        System.out.println("Args: "+args);
+        System.out.println("Kind "+kind);
+        System.out.println("Source location: "+sourceLocation);
+        System.out.println("Target: "+target);
+        System.out.println("Static part: "+staticPart);
+        System.out.println("Long String: "+longString);
+        System.out.println("Short String: "+shortString);
+        System.out.println("String: "+string);
+    }
+
+    @AfterReturning("allService()")
     public void logAfter(JoinPoint joinPoint) {
-        System.err.println("Logging AFTER: " + joinPoint.getSignature());
+        Signature signature = joinPoint.getSignature();
+        Object[] args = joinPoint.getArgs();
+        String kind = joinPoint.getKind();
+        SourceLocation sourceLocation = joinPoint.getSourceLocation();
+        Object target = joinPoint.getTarget();
+        JoinPoint.StaticPart staticPart = joinPoint.getStaticPart();
+        String longString = joinPoint.toLongString();
+        String shortString = joinPoint.toShortString();
+        String string = joinPoint.toString();
+
+        System.out.println("Signature: "+signature);
+        System.out.println("Args: "+args);
+        System.out.println("Kind "+kind);
+        System.out.println("Source location: "+sourceLocation);
+        System.out.println("Target: "+target);
+        System.out.println("Static part: "+staticPart);
+        System.out.println("Long String: "+longString);
+        System.out.println("Short String: "+shortString);
+        System.out.println("String: "+string);
+    }
+
+    @AfterThrowing("allService()")
+    public void logAfterThrowing(JoinPoint joinPoint){
+        Signature signature = joinPoint.getSignature();
+        Object[] args = joinPoint.getArgs();
+        String kind = joinPoint.getKind();
+        SourceLocation sourceLocation = joinPoint.getSourceLocation();
+        Object target = joinPoint.getTarget();
+        JoinPoint.StaticPart staticPart = joinPoint.getStaticPart();
+        String longString = joinPoint.toLongString();
+        String shortString = joinPoint.toShortString();
+        String string = joinPoint.toString();
+
+        System.out.println("Signature: "+signature);
+        System.out.println("Args: "+args);
+        System.out.println("Kind "+kind);
+        System.out.println("Source location: "+sourceLocation);
+        System.out.println("Target: "+target);
+        System.out.println("Static part: "+staticPart);
+        System.out.println("Long String: "+longString);
+        System.out.println("Short String: "+shortString);
+        System.out.println("String: "+string);
     }
 }
